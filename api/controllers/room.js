4612bbb916adefc0973fd28c2b14502b130e1d9c
jackbasso <jackbasso.r@gmail.com>
2022-07-02 13:23:19 -0500

    rooms rutas

---
+++ api/controllers/room.js
@@ -0,0 +1,67 @@
+import Room from "../models/Room.js";
+import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+
+export const createRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  const newRoom = new Room(req.body);
+
+  try {
+    const savedRoom = await newRoom.save();
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $push: {rooms: savedRoom._id},
+      });
+    } catch (err) {
+      next(err)
+    }
+    res.status(200).json(savedRoom);
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const updateRoom = async (req, res, next) => {
+  try {
+    const updatedRoom = await Room.findByIdAndUpdate(req.params.id, { $set: req.body}, //mongoDB $set overwrites the existing value of that field with the value of the specified expression. $set appends new fields to existing documents. You can include one or more $set stages in an aggregation operation.
+     {new:true}) //{new:true} luego de que se actualiza esto retorna la versión actualizada
+    res.status(200).json(updatedRoom)
+  } catch (err) {
+    next();
+  }
+}
+
+export const deleteRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid
+  try {
+    await Room.findByIdAndDelete(req.params.id);
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $pull: {rooms: req.params.id},
+      });
+    } catch (err) {
+      next(err)
+    }
+    res.status(200).json("El Room ha sido eliminado")
+  } catch (err) {
+    next();
+  }
+}
+
+export const getRoom = async (req, res, next) => {
+  try {
+    const room = await Room.findById(req.params.id);
+     res.status(200).json(room);
+  } catch (err) {
+    next();
+  }
+}
+
+export const getRooms = async (req, res, next) => {
+  try {
+    const rooms = await Room.find();
+    res.status(200).json(rooms);
+  } catch (err) {
+    next();
+  }
+}
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,16 +1,19 @@
 import express from 'express';
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from '../controllers/room.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
+
 
 const router = express.Router();
 
 //CREATE
-router.post('/', async (req, res) => {
-  const newHotel = new Hotel(req.body)
-  try {
-    const savedHotel = await newHotel.save()
-    res.status(200).json(savedHotel)
-  } catch (err) {
-    res.status(500).json(err)
-  }
-})
+router.post('/:hotelid', verifyAdmin, createRoom); // ver controller folder room.js
+//UPDATE
+router.put('/:id',  verifyAdmin, updateRoom)
+//DELETE
+router.delete('/:id/:hotelid',  verifyAdmin, deleteRoom)
+//GET
+router.get('/:id', getRoom);
+//GET ALL
+router.get('/', getRooms);
 
 export default router;
