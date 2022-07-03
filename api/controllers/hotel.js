09314cc8ae239bdee44393b78b52c772961b84ca
jackbasso <jackbasso.r@gmail.com>
2022-07-02 19:42:38 -0500

    count by type

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -61,9 +61,20 @@ export const countByCity = async (req, res, next) => {
 }
 
 export const countByType = async (req, res, next) => {
-  try {
-    const hotels = await Hotel.find();
-    res.status(200).json(hotels);
+   try {
+    const hotelCount = await Hotel.countDocuments({type:"hotel"});
+    const apartmentCount = await Hotel.countDocuments({type:"apartment"});
+    const resortCount = await Hotel.countDocuments({type:"resort"});
+    const villaCount = await Hotel.countDocuments({type:"villa"});
+    const cabinCount = await Hotel.countDocuments({type:"cabin"});
+    
+    res.status(200).json([
+      {type:"hotels", count:hotelCount},
+      {type:"apartments", count:apartmentCount},
+      {type:"resorts", count:resortCount},
+      {type:"villas", count:villaCount},
+      {type:"cabins", count:cabinCount},
+    ]);
   } catch (err) {
     next();
   }
