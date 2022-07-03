9e55bce785e7a9cda3dd2c68ef00b9c83c7d688f
jackbasso <jackbasso.r@gmail.com>
2022-07-03 09:50:37 -0500

    propertyList funcionando con el map

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -1,11 +1,11 @@
 import "./propertyList.css";
 import useFetch from "../../hooks/useFetch";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const PropertyList = () => {
-  const [data, setData] = useState([])
-  const {datos, loading, error} = useFetch("/hotels/countByType");
-  setData(datos)
+
+  const {data, loading, error} = useFetch("http://localhost:8800/api/hotels/countByType");
+  
 
   const images = [
     "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
@@ -14,10 +14,11 @@ const PropertyList = () => {
     "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg",
     "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
   ]
+  
 
   return (
     <div className="pList">
-      { loading ? ("loading") : (
+      { loading ? ("loading...") : (
       <>
       { data && images.map((img, i) => (
          <div className="pListItem" key={i}>
@@ -27,8 +28,8 @@ const PropertyList = () => {
           className="pListImg"
         />
         <div className="pListTitles">
-          <h1>{data[0].type}</h1>
-          <h2>{data[0].count} {data[0].type} </h2>
+          <h1>{data[i]?.type}</h1>
+          <h2>{data[i]?.count} {data[i]?.type} </h2>
         </div>
       </div>
       ))}
