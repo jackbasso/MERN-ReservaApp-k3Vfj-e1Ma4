59078a023e2c64103bc675e3260fc16b2846434e
jackbasso <jackbasso.r@gmail.com>
2022-07-06 16:13:56 -0500

    dayDifference function

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -25,6 +25,15 @@ const Hotel = () => {
 
   const {dates} = useContext(SearchContext) //SearchContext.js
 
+  
+  const MILISECONDS_PER_DAY = 1000 * 60 * 60 * 24; // 1000 miliseconds per second
+  //calculate function for days amount between to dates
+  function dayDifference(date1, date2) {
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    const diffDays = Math.ceil(timeDiff / MILISECONDS_PER_DAY);
+    return diffDays;
+  }
+
 
   const handleOpen = (i) => {
     setSlideNumber(i);
