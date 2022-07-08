88687515ef20e9676fa48b27b848a2df84eadc0b
jackbasso <jackbasso.r@gmail.com>
2022-07-08 17:48:30 -0500

    removing submodule

---
+++ admin/src/pages/login/Login.jsx
@@ -0,0 +1,53 @@
+
+import axios from 'axios';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
+import "./login.scss"
+
+const Login = () => {
+  const [ credentials, setCredentials ] = useState({
+    username: undefined,
+    password: undefined
+  })
+  const navigate = useNavigate();
+  const { loading, error, dispatch } = useContext(AuthContext)
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  }
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START "});
+    try {
+      const res = await axios.post('/auth/login', credentials);
+      if(res.data.isAdmin) { // isAdmin viene de mi auth controller del client side
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+      navigate("/") 
+      } else {
+        dispatch({
+          type: "LOGIN_FAILURE",
+          payload: { message: "You are not allowed!"}
+        });
+      }
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data })
+    }
+  };
+
+
+
+  return (
+    <div className='login'>
+      <div className="lContainer">
+        <input type="text" className='lInput' placeholder="username" id="username" onChange={handleChange} />
+        <input type="password" className='lInput' placeholder="password" id="password" onChange={handleChange} />
+        <button disabled={loading} className='lButton' onClick={handleClick}>Login</button>
+        { error && <span>{error.message}</span>}
+      </div>
+    </div>
+  )
+}
+
+export default Login
